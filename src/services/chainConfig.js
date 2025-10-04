59e192c93b415ad2b4be31643d66a6084a559b65
Vipul-sharma119 <vipulsharma924@gmail.com>
2025-10-04 21:35:43 +0530

    fixed network and Url

--- src/services/chainConfig.js
+++ src/services/chainConfig.js
@@ -1,11 +1,11 @@
 export const CHAINS = {
   1: { 
     name: 'Ethereum Mainnet', 
-    rpc: import.meta.env.VITE_ETH_MAINNET_RPC_URL, // Fixed: added _URL
+    rpc: import.meta.env.VITE_ETH_MAINNET_RPC_URL, 
     explorerTx: 'https://etherscan.io/tx/'
   },
   11155111: { 
-    name: 'Sepolia', 
+    name: 'Ethereum Testnet', 
     rpc: import.meta.env.VITE_SEPOLIA_RPC,
     explorerTx: 'https://sepolia.etherscan.io/tx/',
     faucetUrl: 'https://cloud.google.com/application/web3/faucet/ethereum/sepolia'
